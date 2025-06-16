4fbf9404c6159efe83d14439b8e7dcaadf11bbf7
Joris Dobbelsteen <joris@familiedobbelsteen.nl>
2025-06-16 21:58:33 +0200

    Initial template based on blinky

---
+++ README.md
@@ -0,0 +1,8 @@
+# CatPunk app
+
+Microcontroller application for light animations (using a led strip).
+It uses direction and motion to perform light effects.
+
+## Supported platforms
+
+* stm32f411e_disco (for development)
---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#include <app_version.h>
+
+#define SLEEP_TIME_MS 50
+
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void)
+{
+	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+
+  int ret;
+  bool led_state = true;
+
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
+  }
+
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return 0;
+  }
+
+  while (1) {
+    ret = gpio_pin_toggle_dt(&led);
+    if (ret < 0) {
+      return 0;
+    }
+
+    led_state = !led_state;
+    printf("LED state: %s\n", led_state ? "ON" : "OFF");
+    k_msleep(SLEEP_TIME_MS);
+  }
+  return 0;
+}
